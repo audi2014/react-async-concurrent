3b8fc2ca6dedb52ad2bf1d92d5397af1c667bc24
Artur <artur@haulwith.us>
2023-01-10 21:58:36 +0200

    lib init (vite)

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  resolve: {
+    // allow absolute path imports. see tsconfig.json compilerOptions.paths
+    alias: [
+      {
+        find: 'lib',
+        replacement: resolve(__dirname, './lib'),
+      },
+    ],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: '@audi2014/react-async-concurrent',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // exclude react from dist
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [
+    // allow jsx/tsx:
+    react({
+      // exclude react/jsx-runtime (30kb+) from dist
+      jsxRuntime: 'classic',
+    }),
+    // export types:
+    dts(),
+  ],
+});
