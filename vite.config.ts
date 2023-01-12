c61e132db2907e5ac5ff90597cd813aca31e0bb6
Artur <artur@haulwith.us>
2023-01-12 14:53:12 +0200

    fixed vite lib subfolders

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
 import react from '@vitejs/plugin-react';
+import glob from 'glob';
 import { resolve } from 'path';
+import path from 'path';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+import packageJson from './package.dist.json';
 
 // https://vitejs.dev/guide/build.html#library-mode
 // https://onderonur.netlify.app/blog/creating-a-typescript-library-with-vite/
+// https://medium.com/singapore-gds/how-to-support-subpath-imports-using-react-rollup-typescript-1d3d331f821b
+
+// config for each submodule and root package.json
+const subFolderJsonConfigs = glob.sync('./lib/**/', {}).map((folder) => {
+  const distPath = folder.replace('./lib/', '');
+  return {
+    outputFolder: `./dist/${distPath}`,
+    baseContents: distPath
+      ? {
+          name: `${packageJson.name}/${distPath.slice(0, -1)}`,
+          module: './index.js', // --> points to esm format entry point of individual component
+          types: './index.d.ts', // --> points to types definition file of individual component
+          sideEffects: false,
+        }
+      : (packageJson as Required<
+          Parameters<typeof generatePackageJson>
+        >[0]['baseContents']),
+  };
+});
+
+const entriesMask = 'lib/**/*.{ts,tsx}';
+
 export default defineConfig({
   resolve: {
     // allow absolute path imports. see tsconfig.json compilerOptions.paths
@@ -16,20 +44,36 @@ export default defineConfig({
     ],
   },
   build: {
+    minify: true,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      name: '@audi2014/react-async-concurrent',
-      formats: ['cjs'],
-      fileName: (format) => `main.${format}.js`,
+      // include independent modules https://github.com/vitejs/vite/discussions/8098
+      entry: glob.sync(path.resolve(__dirname, entriesMask)),
+      fileName: (format, entryName) => `${entryName}.js`,
     },
     rollupOptions: {
-      // exclude react from dist
+      // exclude react from dist:
       external: ['react'],
-      output: {
-        globals: {
-          react: 'React',
+      output: [
+        {
+          // Keep directory structure and files:
+          preserveModules: true,
+          sourcemap: true,
+          // link *.js.map files to copied lib files. see viteStaticCopy
+          sourcemapPathTransform: (relativeSourcePath) => {
+            return `./${relativeSourcePath.split('/').pop()}`;
+          },
+          // sources already linked in map files. no reason to include sources as strings
+          sourcemapExcludeSources: true,
+          format: 'esm',
+          globals: {
+            react: 'React',
+          },
         },
-      },
+      ],
+      plugins: [
+        // generate package.json for each submodule and root
+        ...subFolderJsonConfigs.map(generatePackageJson),
+      ],
     },
   },
   plugins: [
@@ -39,8 +83,18 @@ export default defineConfig({
       jsxRuntime: 'classic',
     }),
     // export types:
-    dts({
-      rollupTypes: true,
+    // rollupTypes - put all types into one file
+    dts(),
+    // copy sources to dist
+    viteStaticCopy({
+      flatten: false,
+      targets: [
+        {
+          // path of sources which will be copied to dist
+          src: glob.sync(entriesMask),
+          dest: './',
+        },
+      ],
     }),
   ],
 });
